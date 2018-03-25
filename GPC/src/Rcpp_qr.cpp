5a8511f818d4d1f9baae7b9b4a969391865b661b
nasyring <nasyrin@gmail.com>
2018-03-25 10:33:08 -0500

    Update Rcpp_qr.cpp

--- GPC/src/Rcpp_qr.cpp
+++ GPC/src/Rcpp_qr.cpp
@@ -143,11 +143,10 @@ for (int i=0; i<B; i++) {
 bootmean0 = bootmean0/B;
 bootmean1 = bootmean1/B;
 
-
+// create the worker
+GPC_qr_mcmc_parallel gpcWorker(n, ddata, thetaboot, databoot, bootmean0, bootmean1, aalpha, M, B, w, cover);
 while(go) {
-   // create the worker
-   GPC_qr_mcmc_parallel gpcWorker(n, ddata, thetaboot, databoot, bootmean0, bootmean1, aalpha, M, B, w, cover);
-     
+ 
    // call it with parallelFor
    parallelFor(0, B, gpcWorker);
 
