8b8b1c6a42d0ffec1907be897cca3027b8ddde02
nasyring <nasyrin@gmail.com>
2018-03-25 19:47:43 -0500

    Update RcppExports.cpp

--- GPC/src/RcppExports.cpp
+++ GPC/src/RcppExports.cpp
@@ -82,7 +82,7 @@ END_RCPP
 
 arma::colvec rcpp_parallel_qr(SEXP & nn, SEXP & data, SEXP & thetaboot, SEXP & bootmean0, SEXP & bootmean1, SEXP & databoot,
     SEXP & alpha, SEXP & M_samp, SEXP & B_resamp, SEXP & w);
-RcppExport SEXP GPC_rcpp_parallel_qr(SEXP nn, SEXP data, SEXP thetaboot, SEXP bootmean0, SEXP bootmean1, SEXP databoot, SEXP alpha, SEXP M_samp, SEXP B_resamp, SEXP w){
+RcppExport SEXP GPC_rcpp_parallel_qr(SEXP nnSEXP, SEXP dataSEXP, SEXP thetabootSEXP, SEXP bootmean0SEXP, SEXP bootmean1SEXP, SEXP databootSEXP, SEXP alphaSEXP, SEXP M_sampSEXP, SEXP B_resampSEXP, SEXP wSEXP){
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
